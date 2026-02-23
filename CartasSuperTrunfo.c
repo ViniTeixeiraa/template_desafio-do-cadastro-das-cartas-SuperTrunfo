7a975fbc9268dc749bde30149595d40e38399d8e
ViniTeixeiraa <92280263+ViniTeixeiraa@users.noreply.github.com>
2026-02-23 23:00:17 +0000

    Defini as variaveis

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,7 +6,13 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-
+  char EstadoC1, EstadoC2;
+  char CodigoDaCartaC1[4], CodigoDaCartaC2[4];
+  char NomeDaCidadeC1[60], NomeDaCidadeC2[60];
+  int PopulacaoC1, PopulacaoC2;
+  float AreaEmKmC1, AreaEmKmC2;
+  float PibC1, PibC2;
+  int PontosTuristicosC1, PontosTuristicosC2;
   // Área para entrada de dados
 
   // Área para exibição dos dados da cidade
